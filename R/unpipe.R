80a7c81045e0bd668ef7e1ac5c82d33b24b550fc
Ian Cook <icook@cloudera.com>
2019-08-22 21:40:11 -0400

    Add unpipe() function

---
+++ R/unpipe.R
@@ -0,0 +1,25 @@
+# Copyright 2019 Cloudera Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+unpipe <- function(expr) {
+  if (length(expr) == 1) {
+    return(expr)
+  } else {
+    if (expr[[1]] == quote(`%>%`)) {
+      return(as.call(append(as.list(unpipe(expr[[3]])), unpipe(expr[[2]]), after = 1)))
+    } else {
+      return(as.call(lapply(expr, unpipe)))
+    }
+  }
+}
