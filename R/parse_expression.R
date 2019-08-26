3bbb45edd43da8233835cd4d431dc93043b6464e
Ian Cook <icook@cloudera.com>
2019-08-25 22:55:15 -0400

    Improve docs page titles

--- R/parse_expression.R
+++ R/parse_expression.R
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#' SQL expression parser
+#' Parse a SQL expression
 #'
 #' @description Parses a SQL expression into an R expression
 #'
--- R/parse_query.R
+++ R/parse_query.R
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#' SQL query parser
+#' Parse a SQL query
 #'
 #' @description Parses a SQL SELECT statement into a list of R expressions
 #'
