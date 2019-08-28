cfa4c46d1c4fe1261ac70fe8cdffccf762fd7e04
Ian Cook <icook@cloudera.com>
2019-08-28 17:14:49 -0400

    Bugfixes and improvements to aggregate expression tests

--- R/test_expressions.R
+++ R/test_expressions.R
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+are_aggregate_expressions <- function(exprs) {
+  sapply(exprs, is_aggregate_expression)
+}
+
 is_aggregate_expression <- function(expr) {
   if (length(expr) == 1) {
     return(FALSE)
@@ -28,8 +32,12 @@ is_aggregate_expression <- function(expr) {
 # because paste() also is also an aggregate function
 # but only when !is.null(collapse)
 
-is_valid_expression_in_aggregation <- function(expr, group_by, var_names = all_cols(expr), agg = FALSE) {
-  if(deparse(expr) %in% group_by) {
+are_valid_expressions_in_aggregation <- function(exprs, allowed_names) {
+  sapply(exprs, is_valid_expression_in_aggregation, allowed_names = sapply(allowed_names, deparse))
+}
+
+is_valid_expression_in_aggregation <- function(expr, allowed_names, var_names = all_cols(expr), agg = FALSE) {
+  if(deparse(expr) %in% allowed_names) {
     return(TRUE)
   } else if (length(expr) == 1) {
     if (!agg && deparse(expr) %in% var_names) {
@@ -41,7 +49,7 @@ is_valid_expression_in_aggregation <- function(expr, group_by, var_names = all_c
     if (is_aggregate_function_call(expr)) {
       agg <- TRUE
     }
-    out <- lapply(expr, is_valid_expression_in_aggregation, group_by, var_names, agg)
+    out <- lapply(expr, is_valid_expression_in_aggregation, allowed_names, var_names, agg)
   }
   all(sapply(out, isTRUE))
 }
@@ -49,21 +57,21 @@ is_valid_expression_in_aggregation <- function(expr, group_by, var_names = all_c
 is_aggregate_function_call <- function(expr) {
   identical(typeof(expr), "language") && length(expr) > 1 &&
     (deparse(expr[[1]]) %in% r_aggregate_functions ||
-       (expr[[1]] == "paste" && "collapse" %in% names(expr) &&
+       (deparse(expr[[1]]) %in% c("paste","paste0") && "collapse" %in% names(expr) &&
           !is.null(expr[[which(names(expr) == "collapse")]])))
 }
 
 # example usage:
 #
-#group_by <- deparse(quote(Species))
+#allowed_names <- "Species"
 #good <- quote(1 + mean(Sepal.Length + 3))
 #bad <- quote((Sepal.Length + 3) - mean(Sepal.Length + 3))
 #
 #is_aggregate_expression(good)                      # returns TRUE
-#is_valid_expression_in_aggregation(good, group_by) # returns TRUE
+#is_valid_expression_in_aggregation(good, allowed_names) # returns TRUE
 #
 #is_aggregate_expression(good)                      # returns TRUE
-#is_valid_expression_in_aggregation(bad, group_by)  # returns FALSE
+#is_valid_expression_in_aggregation(bad, allowed_names)  # returns FALSE
 #
 
 # note:
