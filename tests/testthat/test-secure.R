39d4b7e0f2e3604e190b7aacb3606ef93ed050fa
Ian Cook <icook@cloudera.com>
2019-08-31 14:42:32 -0400

    Test fix for security vulnerability (#5)

--- tests/testthat/test-secure.R
+++ tests/testthat/test-secure.R
@@ -5,6 +5,14 @@ test_that("parse_expression() fails on illegal function", {
   )
 })
 
+test_that("parse_expression() fails on illegal function when function also used as a column name", {
+  expect_error(
+    parse_expression("system + system('ls')"),
+    "^Unrecognized"
+  )
+})
+
+
 test_that("parse_expression(secure = FALSE) does not fail on illegal function", {
   expect_error(
     parse_expression("system('ls')", secure = FALSE),
