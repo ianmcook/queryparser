af6bf9b2866a7859f01f7011dfd848ca2440d71c
Ian Cook <icook@cloudera.com>
2019-09-06 13:58:52 -0400

    Include indirect function translations in coverage tests

---
+++ codecov.R
@@ -0,0 +1,27 @@
+library(covr)
+
+pkg_result <- package_coverage()
+
+env_result_base <- environment_coverage(
+  env = with(
+    queryparser:::translation_environment_direct_base,
+    queryparser:::translation_environment_indirect_base
+  ),
+  test_files <- paste0("tests/testthat/",list.files("tests/testthat/", pattern = "\\.R$"))
+)
+
+env_result_tidy <- environment_coverage(
+  env = with(
+    queryparser:::translation_environment_direct_tidyverse,
+    queryparser:::translation_environment_indirect_tidyverse
+  ),
+  test_files <- paste0("tests/testthat/",list.files("tests/testthat/", pattern = "\\.R$"))
+)
+
+result <- structure(c(
+  pkg_result,
+  env_result_base,
+  env_result_tidy
+), class = "coverage")
+
+codecov(coverage = result)
