6c13ac462af7e04f10d24bda53845e7b1b7800fa
Ian Cook <icook@cloudera.com>
2020-01-03 20:47:04 -0500

    Add tests for join queries

--- tests/testthat/test-parse_join.R
+++ tests/testthat/test-parse_join.R
@@ -7,6 +7,15 @@ test_that("parse_query() works on SQL-92-style (explicit) join with ON", {
   )
 })
 
+test_that("parse_query() works on SQL-92-style (explicit) join with ON with parentheses", {
+  expect_equal(
+    parse_query("SELECT y.w, z.x FROM y JOIN z ON (y.a = z.b)"),
+      list(select = list(quote(y.w), quote(z.x)), from = structure(list(quote(y),
+      quote(z)), join_types = "inner join", join_conditions = list(quote(y.a ==
+                                                                                                                                     z.b))))
+  )
+})
+
 test_that("parse_query() works on SQL-92-style (explicit) join with USING", {
   expect_equal(
     parse_query("SELECT y.w, z.x FROM y JOIN z USING (a)"),
@@ -26,10 +35,307 @@ test_that("parse_query() works on join with aliases", {
   )
 })
 
+test_that("parse_query() works on CROSS JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x CROSS JOIN y"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "cross join", join_conditions = NA))
+  )
+})
+
+test_that("parse_query() works on NATURAL JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x NATURAL JOIN y"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "natural inner join", join_conditions = NA))
+  )
+})
+
+test_that("parse_query() works on NATURAL INNER JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x NATURAL INNER JOIN y"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "natural inner join", join_conditions = NA))
+  )
+})
+
+test_that("parse_query() works on INNER JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x INNER JOIN y ON x.k = y.k"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "inner join", join_conditions = list(quote(x.k ==
+      y.k))))
+  )
+})
+
+test_that("parse_query() works on NATURAL OUTER JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x NATURAL OUTER JOIN y"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "natural left outer join", join_conditions = NA))
+  )
+})
+
+test_that("parse_query() works on NATURAL LEFT OUTER JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x NATURAL LEFT OUTER JOIN y"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "natural left outer join", join_conditions = NA))
+  )
+})
+
+test_that("parse_query() works on LEFT OUTER JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x LEFT OUTER JOIN y ON x.k = y.k"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "left outer join", join_conditions = list(quote(x.k ==
+      y.k))))
+  )
+})
+
+test_that("parse_query() works on NATURAL RIGHT OUTER JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x NATURAL RIGHT OUTER JOIN y"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "natural right outer join", join_conditions = NA))
+  )
+})
+
+test_that("parse_query() works on RIGHT OUTER JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x RIGHT OUTER JOIN y ON x.k = y.k"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "right outer join", join_conditions = list(quote(x.k ==
+      y.k))))
+  )
+})
+
+test_that("parse_query() works on NATURAL FULL OUTER JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x NATURAL FULL OUTER JOIN y"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "natural full outer join", join_conditions = NA))
+  )
+})
+
+test_that("parse_query() works on FULL OUTER JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x FULL OUTER JOIN y ON x.k = y.k"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "full outer join", join_conditions = list(quote(x.k ==
+        y.k))))
+  )
+})
+
+test_that("parse_query() works on OUTER JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x OUTER JOIN y ON x.k = y.k"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+    quote(y)), join_types = "left outer join", join_conditions = list(quote(x.k ==
+    y.k))))
+  )
+})
+
+test_that("parse_query() works on NATURAL LEFT SEMI JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x NATURAL LEFT SEMI JOIN y"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "natural left semi join", join_conditions = NA))
+  )
+})
+
+test_that("parse_query() works on LEFT SEMI JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x LEFT SEMI JOIN y ON x.k = y.k"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "left semi join", join_conditions = list(quote(x.k ==                                                                                                                                            y.k))))
+  )
+})
+
+test_that("parse_query() works on NATURAL RIGHT SEMI JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x NATURAL RIGHT SEMI JOIN y"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "natural right semi join", join_conditions = NA))
+  )
+})
+
+test_that("parse_query() works on RIGHT SEMI JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x RIGHT SEMI JOIN y ON x.k = y.k"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "right semi join", join_conditions = list(quote(x.k ==                                                                                                                                            y.k))))
+  )
+})
+
+test_that("parse_query() stops on SEMI JOIN without RIGHT or LEFT", {
+  expect_error(
+    parse_query("SELECT a, b FROM x SEMI JOIN y ON x.k = y.k"),
+    "LEFT or RIGHT"
+  )
+})
+
+test_that("parse_query() works on NATURAL LEFT ANTI JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x NATURAL LEFT ANTI JOIN y"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "natural left anti join", join_conditions = NA))
+  )
+})
+
+test_that("parse_query() works on LEFT ANTI JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x LEFT ANTI JOIN y ON x.k = y.k"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "left anti join", join_conditions = list(quote(x.k ==                                                                                                                                            y.k))))
+  )
+})
+
+test_that("parse_query() works on NATURAL RIGHT ANTI JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x NATURAL RIGHT ANTI JOIN y"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "natural right anti join", join_conditions = NA))
+  )
+})
+
+
+test_that("parse_query() works on RIGHT ANTI JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x RIGHT ANTI JOIN y ON x.k = y.k"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "right anti join", join_conditions = list(quote(x.k ==                                                                                                                                            y.k))))
+  )
+})
+
+test_that("parse_query() stops on ANTI JOIN without RIGHT or LEFT", {
+  expect_error(
+    parse_query("SELECT a, b FROM x ANTI JOIN y ON x.k = y.k"),
+    "LEFT or RIGHT"
+  )
+})
+
+test_that("parse_query() works on NATURAL LEFT JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x NATURAL LEFT JOIN y"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "natural left outer join", join_conditions = NA))
+  )
+})
+
+test_that("parse_query() works on LEFT JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x LEFT JOIN y ON x.k = y.k"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "left outer join", join_conditions = list(quote(x.k ==                                                                                                                                       y.k))))
+  )
+})
+
+test_that("parse_query() works on RIGHT JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x RIGHT JOIN y ON x.k = y.k"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "right outer join", join_conditions = list(quote(x.k ==                                                                                                                                       y.k))))
+  )
+})
+
+test_that("parse_query() works on NATURAL FULL JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x NATURAL FULL JOIN y"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "natural full outer join", join_conditions = NA))
+  )
+})
+
+test_that("parse_query() works on FULL JOIN", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x FULL JOIN y ON x.k = y.k"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "full outer join", join_conditions = list(quote(x.k ==                                                                                                                                       y.k))))
+  )
+})
+
+test_that("parse_query() works on JOIN with no modifying keywords", {
+  expect_equal(
+    parse_query("SELECT a, b FROM x JOIN y ON x.k = y.k"),
+    list(select = list(quote(a), quote(b)), from = structure(list(quote(x),
+      quote(y)), join_types = "inner join", join_conditions = list(quote(x.k ==                                                                                                                                       y.k))))
+  )
+})
 
 test_that("parse_query() stops on SQL-89-style (implicit) join", {
   expect_error(
     parse_query("SELECT y.w, z.x FROM y, z WHERE y.a = z.b"),
     "implicit"
+  )
+})
+
+test_that("parse_query() stops on join with repeated AS in table alias", {
+  expect_error(
+    parse_query("SELECT a FROM bee AS AS b NATURAL JOIN see AS c"),
+    "Repeated"
+  )
+})
+
+test_that("parse_query() stops on join with no left table alias after AS", {
+  expect_error(
+    parse_query("SELECT a FROM bee AS NATURAL JOIN see AS c"),
+    "Missing table alias"
+  )
+})
+
+test_that("parse_query() stops on join with no right table alias after AS", {
+  expect_error(
+    parse_query("SELECT a FROM bee AS b NATURAL JOIN see AS"),
+    "Missing table alias"
+  )
+})
+
+test_that("parse_query() stops on join with missing right table reference before ON", {
+  expect_error(
+    parse_query("SELECT a FROM b JOIN ON b.x = c.y"),
+    "Missing table reference"
+  )
+})
+
+test_that("parse_query() stops on join with missing right table reference before USING", {
+  expect_error(
+    parse_query("SELECT a FROM b JOIN USING (z)"),
+    "Missing table reference"
+  )
+})
+
+test_that("parse_query() stops on join no parentheses after USING", {
+  expect_error(
+    parse_query("SELECT y.w, z.x FROM y JOIN z USING a"),
+    "parenthes"
+  )
+})
+
+test_that("parse_query() stops on join no column references in USING clause", {
+  expect_error(
+    parse_query("SELECT y.w, z.x FROM y JOIN z USING ()"),
+    "Missing"
+  )
+})
+
+test_that("parse_query() stops on join with required conditions missing", {
+  expect_error(
+    parse_query("SELECT y.w, z.x FROM y JOIN z"),
+    "conditions"
+  )
+})
+
+test_that("parse_query() stops on NATURAL JOIN with ON clause", {
+  expect_error(
+    parse_query("SELECT a, b FROM x NATURAL JOIN y ON x.k = y.k"),
+    "Unexpected ON"
+  )
+})
+
+test_that("parse_query() stops on NATURAL JOIN with USING clause", {
+  expect_error(
+    parse_query("SELECT a, b FROM x NATURAL JOIN y USING (k)"),
+    "Unexpected USING"
   )
 })
